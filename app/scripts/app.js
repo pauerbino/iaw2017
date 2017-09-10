e9ddf5917f34650211766d17af240c80aeffbbc4
Paula Erbino <Paula.X.Erbino.-ND@disney.com>
2017-09-10 17:57:38 -0300

    first commit iaw2017

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name iaw2017App
+ * @description
+ * # iaw2017App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('iaw2017App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iaw2017App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the iaw2017App
+ */
+angular.module('iaw2017App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
