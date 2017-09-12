9865f2b5fb2e94d8f74dc799ba50991041938ba0
Paula Erbino <Paula.X.Erbino.-ND@disney.com>
2017-09-12 10:27:40 -0300

    contactList para una lista en particular

--- app/scripts/controllers/contactList.controller.js
+++ app/scripts/controllers/contactList.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 angular.module('iaw2017App')
-  .controller('ContactListCtrl', ['$location', '$scope', 'ListService', function ( $location, $scope, ListService) {
-    console.log('fdas');
+  .controller('ContactListCtrl', ['$location', '$routeParams', '$scope', 'ListService',
+    function ( $location, $routeParams, $scope, ListService) {
+
+    $scope.list = {};
+
+    function initialize() {
+        $scope.list = ListService.getList($routeParams.id);
+    }
+
+    initialize();
 
   }]);
--- app/scripts/services/list.service.js
+++ app/scripts/services/list.service.js
@@ -1,9 +1,66 @@
 'use strict';
 angular.module('iaw2017App')
     .service('ListService', function () {
-        var lists = [{name: 'List1', id: 1},{name: 'List2', list: 2}];
+        var lists = [
+            {
+                name: 'List1',
+                id: 1,
+                contacts: [
+                    {
+                        id: 1,
+                        name: "John",
+                        lastName: "Mayer",
+                        email: "johnmayer@gmail.com",
+                        phone: "4525792"
+                    },
+                    {
+                        id: 2,
+                        name: "Boris",
+                        lastName: "Grey",
+                        email: "borisgrey@gmail.com",
+                        phone: "4525792"
+                    },
+                    {
+                        id: 3,
+                        name: "Emma",
+                        lastName: "Black",
+                        email: "emmablack@gmail.com",
+                        phone: "4525792"
+                    }
+                ]
+            },{
+                name: 'List2',
+                list: 2,
+                contacts: [
+                    {
+                        id: 1,
+                        name: "John",
+                        lastName: "Mayer",
+                        email: "johnmayer@gmail.com",
+                        phone: "4525792"
+                    },
+                    {
+                        id: 2,
+                        name: "Boris",
+                        lastName: "Grey",
+                        email: "borisgrey@gmail.com",
+                        phone: "4525792"
+                    },
+                    {
+                        id: 3,
+                        name: "Emma",
+                        lastName: "Black",
+                        email: "emmablack@gmail.com",
+                        phone: "4525792"
+                    }
+                ]
+            }];
 
         this.getLists = function() {
             return lists;
         };
+
+        this.getList = function(id) {
+            return lists[0];
+        }
     });
